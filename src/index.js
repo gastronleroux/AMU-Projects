3abf693174d7f58e7b21025d186046e050b98258
gastronleroux <dejmieniv@gmail.com>
2021-06-24 16:51:48 +0200

    add 'choose project' info

--- src/index.js
+++ src/index.js
@@ -4,8 +4,11 @@ document.getElementById("log").addEventListener( "mouseover", () => changeMaxWid
 document.getElementById("emp").addEventListener( "mouseover", () => changeMaxWidth('emp', ['wrp', 'ana', 'log']) )
 
 function changeMaxWidth(intoNone, intoInt, n = 150){
-    document.getElementById(intoNone).style.width = 'inherit';
-    for (let i = 0; i < intoInt.length; i++){
-        document.getElementById(intoInt[i]).style.width = n;
+    if(document.getElementById(intoNone).style.width != 'inherit'){
+        document.getElementById(intoNone).style.width = 'inherit';
+        for (let i = 0; i < intoInt.length; i++){
+            document.getElementById(intoInt[i]).style.width = n;
+        }
+        document.getElementById("emp").innerHTML = '';
     }
 }
