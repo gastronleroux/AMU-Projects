981534b0ad034953b69271935f7f82593088ca4a
gastronleroux <dejmieniv@gmail.com>
2021-06-24 16:30:48 +0200

    add routing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-document.getElementById("wrp").addEventListener( "mouseover", () => changeMaxWidth('wrp', ['ana', 'log']) )
-document.getElementById("ana").addEventListener( "mouseover", () => changeMaxWidth('ana', ['wrp', 'log']) )
-document.getElementById("log").addEventListener( "mouseover", () => changeMaxWidth('log', ['wrp', 'ana']) )
+document.getElementById("wrp").addEventListener( "mouseover", () => changeMaxWidth('wrp', ['ana', 'log', 'emp']) )
+document.getElementById("ana").addEventListener( "mouseover", () => changeMaxWidth('ana', ['wrp', 'log', 'emp']) )
+document.getElementById("log").addEventListener( "mouseover", () => changeMaxWidth('log', ['wrp', 'ana', 'emp']) )
 document.getElementById("emp").addEventListener( "mouseover", () => changeMaxWidth('emp', ['wrp', 'ana', 'log']) )
 
 function changeMaxWidth(intoNone, intoInt, n = 150){
